f88d3f18f0876baf539443b2f74beb3d611c43fd
minwook <vxel7408@gmail.com>
2022-10-14 19:00:20 +0900

    1

---
+++ src/기본/ProjectMethod/FirstMethod.java
@@ -0,0 +1,12 @@
+package 기본.ProjectMethod;
+
+public class FirstMethod {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Method");
+        System.out.println(Math.floor(1.1));
+
+    }
+
+}
---
+++ src/기본/ProjectMethod/WhyMethod.java
@@ -0,0 +1,7 @@
+package 기본.ProjectMethod;
+
+public class WhyMethod {
+
+
+
+}
