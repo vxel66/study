4cc5a8eaf86a32c93c0c5539deb9494005d8c296
minwook <vxel7408@gmail.com>
2023-03-01 12:55:40 +0900

    1

---
+++ src/기본/Baeakjoon/입출력_사칙연사/꼬마_정민이.java
@@ -0,0 +1,13 @@
+package 기본.Baeakjoon.입출력_사칙연사;
+
+import java.util.Scanner;
+
+public class 꼬마_정민이 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        String input = s.nextLine();
+
+    }
+}
