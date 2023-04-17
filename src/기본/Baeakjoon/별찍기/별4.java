7f54ef275ac4a9cbffae1a21a6d648b92a2c78ee
minwook <vxel7408@gmail.com>
2023-04-17 18:11:12 +0900

    1

---
+++ src/기본/Baeakjoon/별찍기/별4.java
@@ -0,0 +1,18 @@
+package 기본.Baeakjoon.별찍기;
+
+public class 별4 {
+
+    public static void main(String[] args) {
+        for(int i=0; i<4; i++){
+            for(int j=0;j<3-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+    }
+
+}
